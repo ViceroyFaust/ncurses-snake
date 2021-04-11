275eb3754f62c6ee6b6c185d539f9d07cba21ad0
Danylo Owl <d.ribchinski@gmail.com>
2021-04-11 09:13:22 -0400

    made menu

--- main.cpp
+++ main.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
+#include <string.h>
+#include <ncurses.h>
 
 using namespace std;
 
+void cursesInit() {
+    initscr();
+    raw(); // no line buffering
+    keypad(stdscr, true); //enable keypad and special keys
+    noecho(); // Don't print input
+}
+
+void clean() {
+    noraw(); //Disable raw for reasons
+    endwin();
+}
+
+void displayMenu() {
+    int row, col;
+    getmaxyx(stdscr, row, col);
+    char welcome[] = "Welcome to C++ Snake!";
+    char prsAnyKey[] = "Press any key to start";
+    char signature[] = "Created by Danylo Rybchynskyi, 2021";
+    mvprintw(row/2, (col-strlen(welcome))/2, "%s", welcome);
+    mvprintw(row/2-1, (col-strlen(prsAnyKey))/2, "%s", prsAnyKey);
+    mvprintw(row-1, 0, "%s", signature);
+    getch();
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    cursesInit();
+    displayMenu();
+    clean();
 }
