c5a0f7f3e09f7e0fe81f73ca9b25a6e4f8d8377e
Danylo Owl <d.ribchinski@gmail.com>
2021-04-17 17:24:25 -0400

    made rudimentary rect

--- main.cpp
+++ main.cpp
@@ -2,13 +2,12 @@
 #include <string.h>
 #include <ncurses.h>
 
-using namespace std;
-
 void cursesInit() {
     initscr();
     raw(); // no line buffering
     keypad(stdscr, true); //enable keypad and special keys
     noecho(); // Don't print input
+    curs_set(0);
 }
 
 void clean() {
@@ -26,11 +25,33 @@ void displayMenu() {
     mvprintw(row/2-1, (col-strlen(prsAnyKey))/2, "%s", prsAnyKey);
     mvprintw(row-1, 0, "%s", signature);
     getch();
+    clear();
+}
+
+void drawBox(int x, int y) {
+    addch(ACS_ULCORNER);
+    for (int i = 0; i < x-2; i++) {
+        addch(ACS_HLINE);
+    }
+    addch(ACS_URCORNER);
+
+    for(int i = 0; i < y-2; i++) {
+        mvaddch(getcury(stdscr)+1, 0, ACS_VLINE);
+        mvaddch(getcury(stdscr), x-1, ACS_VLINE);
+    }
+    move (getcury(stdscr)+1, 0);
+    addch(ACS_LLCORNER);
+    for (int i = 0; i < x-2; i++) {
+        addch(ACS_HLINE);
+    }
+    addch(ACS_LRCORNER);
+    getch();
 }
 
 int main()
 {
     cursesInit();
     displayMenu();
+    drawBox(32, 32);
     clean();
 }
