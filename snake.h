d1ff8c86845f49494f1ca9dcd37e4dc8a87be90e
Danylo Owl <d.ribchinski@gmail.com>
2021-04-29 11:49:50 -0400

    created BodyPart struct

--- snake.h
+++ snake.h
@@ -1,6 +1,17 @@
 #ifndef SNAKE_H_INCLUDED
 #define SNAKE_H_INCLUDED
 
+struct BodyPart {
+    enum Direction {
+        d_up;
+        d_down;
+        d_left;
+        d_right;
+    };
 
+    Direction movDirection{};
+    int x{};
+    int y{};
+};
 
 #endif // SNAKE_H_INCLUDED
