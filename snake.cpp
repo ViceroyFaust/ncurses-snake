2296ea23557b7b9ce96450dfcfbaac853f41f26a
Danylo Owl <d.ribchinski@gmail.com>
2021-04-30 16:49:39 -0400

    made method for moving the snake

--- snake.cpp
+++ snake.cpp
@@ -12,7 +12,24 @@ void Snake::grow() {
 }
 
 void Snake::move() {
-
+    for (BodyPart part : m_body) {
+        switch(part.movDirection) {
+            /* The coordinate plane starts at 0,0 in
+               the upper left corner */
+            case d_up:
+                part.y -= 1;
+                break;
+            case d_down:
+                part.y += 1;
+                break;
+            case d_left:
+                part.x -= 1;
+                break;
+            case d_right:
+                part.x += 1;
+                break;
+        }
+    }
 }
 
 void Snake::setDirection(Direction& direction) {
