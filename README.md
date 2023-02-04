e98c98c903e0948e59115f6551f0ec7ed8113adb
Danylo Rybchynskyi <d.ribchinski@gmail.com>
2023-02-04 14:26:36 +0200

    Provide the user with optional arena size

--- README.md
+++ README.md
@@ -2,7 +2,12 @@
 This is a simple ncurses snake implementation in C++.
 
 ## Compiling
-Simply run 'make' in your terminal and run the executable! The game uses ncurses, so make sure that the library is installed on your system. As far as I am aware, ncurses is Unix only.
+Simply run `make` in your terminal and run the executable! The game uses ncurses, so make sure that the library is installed on your system. As far as I am aware, ncurses is Unix only.
+
+## Running
+When you run the executable, you will get a standard 16x16 snake game. If you wish to specify a custom arena size, add the following argument:
+
+`SnakeGame --size [width] [height]`
 
 ## Copyright
 © 2021 Danylo Rybchynskyi
--- main.cpp
+++ main.cpp
@@ -1,6 +1,42 @@
 #include "terminal.h"
+#include <iostream>
+#include <vector>
+#include <string>
 
-int main() {
+bool isPositiveNumber(const std::string& str) {
+    for (char c : str) {
+        if (c < '0' || c > '9')
+            return false;
+    }
+    return true;
+}
+
+int main(int argc, char* argv[]) {
+    // Copy all of the arguments into a vector of strings
+    std::vector<std::string> arguments(argv, argv + argc);
+    if (argc > 1) {
+        std::string argument = arguments[1];
+        if (argument == "-h" || argument == "--help") {
+            std::cout << "=== Snake Game Help ===\n"
+                << "Run the executable with no parameters for standard game\n"
+                << "--size [width] [height]: run the game with custom arena size\n"
+                << "-h or --help: print this help screen\n";
+            return 0;
+        }
+        if (argument == "--size") {
+            if (argc != 4 ||
+                    (!isPositiveNumber(arguments[2]) || !isPositiveNumber(arguments[3]))) {
+                std::cout << "Error: Improper --size arguments!\n";
+                return -1;
+            }
+            Terminal term = Terminal(std::stoi(arguments[2]), std::stoi(arguments[3]));
+            term.startGame();
+            return 0;
+        }
+        std::cout << "Error: Improper arguments!\n";
+        return -1;
+    }
+
     Terminal term = Terminal(16, 16);
     term.startGame();
     return 0;
